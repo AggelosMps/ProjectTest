b27271c07a2af18483f67871cc02f860200ef5b4
Άγγελος Μπάσο <148365901+AggelosMps@users.noreply.github.com>
2023-11-04 11:54:46 +0200

    Begining

--- ProjectTest.java
+++ ProjectTest.java
@@ -1,36 +1,15 @@
 public class ProjectTest {
     public static void main(String[] args) {
-        System.out.println("This is a test");
-    }
-
-    public int Ath(){
-        return 69;
-    }
-    public String Zaza(){
-        return "Eimai kaif";
-    }
-
-    public static void getNikos() {
-        System.out.println("German tourists oso zw mazwi");
-   }
-
-    public static void epitelousmphkaapocommandline() {
-        System.out.println("PAMEEEE LIGO");
-    }
-
-    public static void getKOK() {
-        System.out.println("edhcuhe");
-    }
-
-   
     
-    public String Loco(){
-        return "pita soublaki tzatziki kai tmax";
-            }
-    public void KostaoKavlas() {
-	    System.out.println("Kostas");
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     }
-    public static void hello() {
-        System.out.println("Hello");
-    } 
 }
