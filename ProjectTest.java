28ef54e4a1427b3002c04021495f54cbe10f9ea0
Άγγελος Μπάσο <148365901+AggelosMps@users.noreply.github.com>
2023-11-05 12:22:45 +0200

    Update ProjectTest.java

--- ProjectTest.java
+++ ProjectTest.java
@@ -1,14 +1,9 @@
 public class ProjectTest {
     public static void main(String[] args) {
-        System.out.println("This is a test");
-    }
-
-    public int Ath(){
-        return 69;
-    }
-    public String Zaza(){
-        return "Eimai kaif";
-    }
-   
     
+    
+    
+    
+    
+    }   
 }
