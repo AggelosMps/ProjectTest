d7a99deb214310421d64417a664dc23c5750b192
= <=>
2023-11-13 22:41:25 +0200

    File input/output

---
+++ File.java
@@ -0,0 +1,31 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class File {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] names = {"Vasilis","Dimitris","Aggelos","Kostas","Stratos","Mpampis","Nikos"};
+
+        //Μπορείς το αρχείο που δημιουργείς να το βάλεις και σε φάκελο ως εξής: C:\\Users\\nickkantzaris\\Applications\\Programming
+
+        System.out.println("Give your username");
+        String username = sc.nextLine();
+        System.out.println("Give your password");
+        String password = sc.nextLine();
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(username+".txt"));
+            writer.write("Username:" +username);
+            writer.write("\nPassword:" + password);
+            for (String name : names) {
+                writer.write("\n" + name);
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
