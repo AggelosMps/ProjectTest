50c7da506e871db3fcc9aa14ec5245967a0e0def
= <=>
2023-11-20 22:52:02 +0200

    Add filewriter to username and password

--- ManagerData.java
+++ ManagerData.java
@@ -1,3 +1,7 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class ManagerData {
     private String username;
     private String password;
@@ -8,10 +12,21 @@ public class ManagerData {
     public ManagerData(String username, String password) {
         this.username = username;
         this.password = password;
+	try {
+	   BufferedWriter writer = new BufferedWriter(new FileWriter(this.username + "1.txt"));//Δημιουργία ενός αρχείου για την ταυτοποίηση του χρήστη
+	   writer.write(this.username);
+	   writer.write("n\" + this.password);
+	   writer.close();
+	} catch (IOException e) {
+	   e.printStackTrace();
+	}
+
     }
+§
     public double getIncome() {
         return income;
     }
+
     public void SetIncome(double income) {
         this.income=income;
     }
