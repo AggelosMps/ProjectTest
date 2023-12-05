19e34a8c0b6eadfc859c158b9e9e3f611bd0e7e4
= <=>
2023-12-05 18:10:58 +0200

    Options for User

---
+++ demo/src/main/java/com/example/Options.java
@@ -0,0 +1,33 @@
+package com.retail;\\Αλλαγή του πακέτου 
+import com.retail.*;\\Αλλαγή για το πακέτο μας
+
+public class Options {
+    public static void choice1() {
+        String username = MethodsUser.nameUser();
+        String password = MethodsUser.passwordUser();
+        if (UseDB.authenticateUser(username, password)) {
+            System.out.println("Login successful!");
+            if (UseDB.checkfores(username) == 0) {
+                UseDB.elegxossyndesis(username);
+                MethodsUser.question1(username);
+                MethodsUser.question2(username);
+                MethodsUser.question3(username);
+            } 
+        } else {
+            System.out.println("Login failed.Incorrect username or password.");
+        }
+    }
+    public static void choice2() {
+        String username = MethodsUser.nameUser();
+        if (UseDB.usernameExist(username)) {
+            System.out.println("Username already taken. Please choose another.");
+        } else {
+            String password = MethodsUser.passwordUser();
+            System.out.println("Registration successful! You can now log in.");
+            MethodsUser.newuser(username, password);
+        }
+    }
+    public static void choice3() {
+        System.out.println("Login failed.Incorrect username or password.");
+    }
+}
