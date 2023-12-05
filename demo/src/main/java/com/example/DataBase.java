daee2b7d8145a6c696d6e0d327e4d8a5adf73fef
= <=>
2023-12-05 22:07:25 +0200

    Change methods select and int and double

--- demo/src/main/java/com/example/DataBase.java
+++ demo/src/main/java/com/example/DataBase.java
@@ -3,32 +3,25 @@ import java.sql.*;
 
 public class DataBase {
     public static Connection connection = null;
-
-    public static int selectFromTable(String username, String apantisi) {
+    public static Number selectFromTable(String username, String apantisi) {
         try {
             connection = DriverManager.getConnection("jdbc:sqlite:revenue_data.db");
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT " + apantisi + " FROM Manager WHERE username = '" + username + "'");
+            ResultSet resultSet = statement.executeQuery("SELECT " + apantisi + " FROM Manager WHERE username = " + "'" + username + "'");
             if (resultSet.next()) {
-                int temp = resultSet.getInt(apantisi);
-                return temp;
+                if (resultSet.getObject(apantisi) instanceof Integer) {
+                    return resultSet.getInt(apantisi);
+                } else if (resultSet.getObject(apantisi) instanceof Double) {
+                    return resultSet.getDouble(apantisi);
+                }
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
         return 0;
     }
 
     public static void main(String[] args) {
-        int i = selectFromTable("Nikos", "ap_1");
-        System.out.println(i);
+        System.out.println(selectFromTable("Nikos", "ap_1"));
     }
 }
